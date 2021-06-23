599b61782e2b9089ab764230e8db50b669a3845d
RuanAlves <ferreira.bessa@hotmail.com>
2021-06-23 08:12:08 -0300

    mudanças

--- lib/telefonia.ex
+++ lib/telefonia.ex
@@ -1,10 +1,80 @@
 defmodule Telefonia do
+
+  @moduledoc """
+  Módulo possuí funções que fazem parse para as demais funções de outros módulos do sistema
+  """
+
+  @doc """
+  Função cria arquivos `pre.txt` e `pos.txt`, esses arquivos recebem dados dos assinantes
+
+  ## Exemplo
+
+    iex> Telefonia.start()
+    :ok
+
+  """
   def start do
     File.write("pre.txt", :erlang.term_to_binary([]))
     File.write("pos.txt", :erlang.term_to_binary([]))
   end
 
+
+  @doc """
+  Função parse para `Assinante.cadastrar/4`
+
+  """
   def cadastrar_assinante(nome, numero, cpf, plano) do
     Assinante.cadastrar(nome, cpf, numero, plano)
   end
+
+  @doc """
+  Função parse para `Assinante.assinantes/0`
+  """
+  def listar_assinantes, do: Assinante.assinantes()
+
+  @doc """
+  Função parse para `Assinante.assinantes_prepago/0`
+
+  """
+  def listar_assinantes_prepago, do: Assinante.assinantes_prepago()
+
+  @doc """
+  Função parse para `Assinante.assinantes_pospago/0`
+  """
+  def listar_assinantes_pospago, do: Assinante.assinantes_postpago()
+
+  def fazer_chamada(numero, plano, data, duracao) do
+    cond do
+      plano == :prepago -> Prepago.fazer_chamada(numero, data, duracao)
+      plano == :pospago -> Pospago.fazer_chamada(numero, data, duracao)
+    end
+  end
+
+  def recarga(numero, data, valor), do: Recarga.nova(data, valor, numero)
+
+  def buscar_por_numero(numero, plano \\ :all), do: Assinante.buscar_assinante(numero, plano)
+
+  def imprimir_contas(mes, ano) do
+
+    Assinante.assinantes_prepago()
+    |> Enum.each(fn assinante ->
+      assinante = Prepago.imprimir_conta(mes, ano, assinante.numero)
+      IO.puts("Conta Prepaga do Assinante: #{assinante.nome}")
+      IO.puts("Número: #{assinante.numero}")
+      IO.puts("Chamadas: ")
+      IO.puts(assinante.chamadas)
+      IO.puts("Recargas: ")
+      IO.inspect(assinante.plano.recargas)
+      IO.puts("Total de Chamadas: #{Enum.count(assinante.chamadas)}")
+      IO.puts("Total de Recargas: #{Enum.count(assinante.plano.recargas)}")
+      IO.puts("=================================================================")
+    end)
+
+    Assinante.assinantes_postpago()
+    |> Enum.each(fn assinante ->
+
+    end)
+
+  end
+
 end
---
+++ test/pospago_test.exs
@@ -0,0 +1,55 @@
+defmodule PospagoTest do
+
+  use ExUnit.Case
+  doctest Pospago
+
+  #Toda vez que for realizar um teste, passa no SETUP primeito
+  #Assim irá criar os arquivos .txt, e no 'on_exit' irá apagar esses arquivos, depois de terminar o teste
+  setup do
+
+    File.write("pre.txt", :erlang.term_to_binary([]))
+    File.write("pos.txt", :erlang.term_to_binary([]))
+
+    on_exit(fn ->
+      File.rm("pre.txt")
+      File.rm("pos.txt")
+    end)
+
+  end
+
+  test "Deve testar a estrutura" do
+    assert %Pospago{valor: 10}.valor == 10
+  end
+
+  test "Deve fazer uma ligação" do
+
+    Assinante.cadastrar("Usuario", "123", "123", :pospago)
+
+    assert Pospago.fazer_chamada("123", DateTime.utc_now(), 5) ==
+      {:ok, "Chamada feita com sucesso! duracao: 5 minutos"}
+
+  end
+
+  test "Deve imprimir a conta do assinante" do
+
+    Assinante.cadastrar("Usuario", "123", "123", :pospago)
+
+    data = DateTime.utc_now()
+    data_antiga = ~U[2021-05-21 16:09:01.241846Z]
+
+    Pospago.fazer_chamada("123", data, 3)
+    Pospago.fazer_chamada("123", data_antiga, 3)
+    Pospago.fazer_chamada("123", data, 3)
+    Pospago.fazer_chamada("123", data, 3)
+
+    assinante = Assinante.buscar_assinante("123", :pospago)
+    assert Enum.count(assinante.chamadas) == 4
+
+    assinante = Pospago.imprimir_conta(data.month, data.year, "123")
+    assert assinante.numero == "123"
+    assert Enum.count(assinante.chamadas) == 3
+    assert assinante.plano.valor == 12.599999999999998
+
+  end
+
+end
